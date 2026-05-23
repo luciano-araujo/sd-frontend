aeb8a848848502b415219fd763ae31b65b148842
Luciano Araujo <araujo.luciano1302@gmail.com>
2026-05-23 16:11:15 -0300

    env variable

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { useEffect, useMemo, useState } from 'react'
 import './App.css'
 
-const API_URL = 'http://localhost:8080/catalogo'
+const API_URL = import.meta.env.VITE_SD_API_URL
 
 const emptyForm = {
   nome: '',
